100a97ca95a7b61e6805e385f57580145dfe68d6
maple <maple@itcast.cn>
2018-09-09 19:33:45 +0800

    first commit

---
+++ src/com/itheima/demo01/SumTest02.java
@@ -0,0 +1,13 @@
+package com.itheima.demo01;
+
+public class SumTest02 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i = 0; i < 10; i++) {
+			sum+=i;
+			System.out.println(sum);
+		}
+	}
+
+}
---
+++ src/com/itheima/demo01/SumText01.java
@@ -0,0 +1,17 @@
+package com.itheima.demo01;
+
+public class SumText01 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+"*"+j+"="+(i*j)+"\t");
+			}
+			System.out.println();
+		
+			
+		}
+
+	}
+
+}
